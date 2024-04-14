75affd0b8262711baed9bac5b630eae391587fd5
Nikunj Gupta <nikunj9180@gmail.com>
2024-04-14 23:07:26 +0530

    initial commit

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('contact-btn');
+const btn2 = document.getElementById('submit');
+const card =document.getElementById('contact-form')
+
+
+btn.addEventListener('click', () => {
+   card.classList.add("active");
+});
+
